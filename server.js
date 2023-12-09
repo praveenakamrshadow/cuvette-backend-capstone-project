87d51d1bf9cc9c3aa39501acdd6b97a5f0f6a026
praveen Sahu <praveen.pkyt327867@gmail.com>
2023-12-09 21:13:14 +0530

    Added database config and initialization

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
+});
+
+app.get("/health", (req, res) => {
+    res.json({ status: "Server is up and running" });
+});
+
+mongoose.connect(
+    `mongodb+srv://admin:1234@cluster0.qflosab.mongodb.net/?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+    console.log("Connected to MongoDB");
 });
 
 app.listen(PORT, () => {
